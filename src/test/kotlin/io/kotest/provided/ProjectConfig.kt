269ea30b6c7928d5102f06463b5ecdebb42d7150
Krystian Kryszczak <krystiankryszczak725@wp.pl>
2023-08-10 23:24:44 +0200

    Update Kotest ProjectConfig.

--- src/test/kotlin/io/kotest/provided/ProjectConfig.kt
+++ src/test/kotlin/io/kotest/provided/ProjectConfig.kt
@@ -3,12 +3,17 @@ package io.kotest.provided
 import io.kotest.core.config.AbstractProjectConfig
 import io.micronaut.test.extensions.kotest5.MicronautKotest5Extension
 import org.testcontainers.containers.CassandraContainer
+import org.testcontainers.containers.wait.strategy.Wait
 import org.testcontainers.utility.DockerImageName
+import java.time.Duration
 
 object ProjectConfig : AbstractProjectConfig() {
     override fun extensions() = listOf(MicronautKotest5Extension)
 
     private val cassandra = CassandraContainer(DockerImageName.parse("cassandra:3.11.2"))
+        .waitingFor(Wait.forListeningPort()
+        .withStartupTimeout(Duration.ofSeconds(30)))
+        .withExposedPorts(9042)
 
     override suspend fun beforeProject() {
         cassandra.start()
