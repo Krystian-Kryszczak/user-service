e96e0c4dbb5cae4019327eb955acc9c0e1749a8b
Krystian Kryszczak <krystiankryszczak725@wp.pl>
2023-08-10 04:18:50 +0200

    Add and configure the TestContainers Cassandra module.

--- src/test/kotlin/io/kotest/provided/ProjectConfig.kt
+++ src/test/kotlin/io/kotest/provided/ProjectConfig.kt
@@ -2,7 +2,19 @@ package io.kotest.provided
 
 import io.kotest.core.config.AbstractProjectConfig
 import io.micronaut.test.extensions.kotest5.MicronautKotest5Extension
+import org.testcontainers.containers.CassandraContainer
+import org.testcontainers.utility.DockerImageName
 
 object ProjectConfig : AbstractProjectConfig() {
     override fun extensions() = listOf(MicronautKotest5Extension)
+
+    private val cassandra = CassandraContainer(DockerImageName.parse("cassandra:3.11.2"))
+
+    override suspend fun beforeProject() {
+        cassandra.start()
+    }
+
+    override suspend fun afterProject() {
+        cassandra.stop()
+    }
 }
