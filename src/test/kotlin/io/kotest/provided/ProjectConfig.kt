74ee7674bc90272f93bd97b5051b7c78bee3e1ad
Krystian Kryszczak <krystiankryszczak725@wp.pl>
2023-08-11 03:03:32 +0200

    Update Kotest ProjectConfig.

--- src/test/kotlin/io/kotest/provided/ProjectConfig.kt
+++ src/test/kotlin/io/kotest/provided/ProjectConfig.kt
@@ -1,8 +1,12 @@
 package io.kotest.provided
 
+import com.datastax.driver.core.Cluster
+import com.datastax.driver.core.Session
+import com.datastax.oss.driver.api.querybuilder.SchemaBuilder
 import io.kotest.core.config.AbstractProjectConfig
 import io.micronaut.test.extensions.kotest5.MicronautKotest5Extension
 import org.testcontainers.containers.CassandraContainer
+import org.testcontainers.containers.CassandraContainer.CQL_PORT
 import org.testcontainers.containers.wait.strategy.Wait
 import org.testcontainers.utility.DockerImageName
 import java.time.Duration
@@ -11,12 +15,28 @@ object ProjectConfig : AbstractProjectConfig() {
     override fun extensions() = listOf(MicronautKotest5Extension)
 
     private val cassandra = CassandraContainer(DockerImageName.parse("cassandra:3.11.2"))
-        .waitingFor(Wait.forListeningPort()
-        .withStartupTimeout(Duration.ofSeconds(30)))
+        .waitingFor(Wait.forListeningPort().withStartupTimeout(Duration.ofSeconds(30)))
         .withExposedPorts(9042)
+
+    private fun getSession(): Session = Cluster.builder()
+        .addContactPoint(cassandra.host)
+        .withPort(cassandra.getMappedPort(CQL_PORT))
+        .withoutJMXReporting()
+        .build()
+        .connect()
 
     override suspend fun beforeProject() {
         cassandra.start()
+
+        getSession().execute(
+            SchemaBuilder.createKeyspace(System.getProperty("CASSANDRA_KEYSPACE", "app")).ifNotExists()
+                .withSimpleStrategy(2)
+                .build()
+                .query
+        )
+
+        System.setProperty("CASSANDRA_HOST", cassandra.host)
+        System.setProperty("CASSANDRA_PORT", cassandra.getMappedPort(9042).toString())
     }
 
     override suspend fun afterProject() {
