a5f44f390379b2d18570b9e91b7bf2474e1287f6
Krystian Kryszczak <krystiankryszczak725@wp.pl>
2023-09-04 04:06:25 +0200

    Update friend service implementation.

--- src/main/kotlin/krystian/kryszczak/service/friend/FriendServiceImpl.kt
+++ src/main/kotlin/krystian/kryszczak/service/friend/FriendServiceImpl.kt
@@ -6,6 +6,7 @@ import io.reactivex.rxjava3.core.Maybe
 import io.reactivex.rxjava3.core.Single
 import jakarta.inject.Singleton
 import krystian.kryszczak.commons.model.being.user.User
+import krystian.kryszczak.commons.service.being.user.UserService
 import krystian.kryszczak.commons.utils.SecurityUtils
 import krystian.kryszczak.model.invitation.FriendInvitation
 import krystian.kryszczak.model.invitation.FriendInvitationModel
@@ -14,16 +15,29 @@ import krystian.kryszczak.storage.cassandra.dao.invitation.FriendInvitationDao
 import java.util.UUID
 
 @Singleton
-class FriendServiceImpl(private val friendDao: FriendDao, private val friendInvitationDao: FriendInvitationDao): FriendService {
+class FriendServiceImpl(
+    private val userService: UserService,
+    private val friendDao: FriendDao,
+    private val friendInvitationDao: FriendInvitationDao
+): FriendService {
     override fun propose(authentication: Authentication): Flowable<User> {
         return propose(SecurityUtils.getClientId(authentication) ?: return Flowable.empty())
     }
 
-    override fun propose(clientId: UUID) = Flowable.fromPublisher(friendDao.findWithoutUserIdInFriends(clientId))
+    override fun propose(clientId: UUID): Flowable<User> =
+        userService.findById(clientId)
+        .map(User::friends)
+        .flatMapPublisher { friends ->
+            Flowable.fromIterable(friends)
+                .flatMapMaybe(userService::findById)
+                .flatMapIterable(User::friends)
+                .skipWhile(friends::contains)
+                .flatMapMaybe(userService::findById)
+        }
 
     override fun friendshipList(page: Int, clientId: UUID) =
         Maybe.fromPublisher(friendDao.findById(clientId))
-            .map { it.friends }
+            .map(User::friends)
             .flatMapPublisher { Flowable.fromIterable(it) }
 
     override fun invitations(id: UUID): Flowable<FriendInvitation> = Flowable.fromPublisher(friendInvitationDao.findByReceiverId(id))
@@ -38,13 +52,11 @@ class FriendServiceImpl(private val friendDao: FriendDao, private val friendInvi
             .filter { it.inviter != null && it.receiver != null }
             .flatMap {
                 Maybe.fromPublisher(friendInvitationDao.deleteIfExist(invitation.mapToInvitation()))
-            }
-            .filter { it }
+            }.filter { it }
             .doOnSuccess {
                 friendDao.addFriend(invitation.inviter!!, invitation.receiver!!)
                 friendDao.addFriend(invitation.receiver, invitation.inviter)
-            }
-            .subscribe()
+            }.subscribe()
     }
 
     override fun acceptInvitation(id: UUID) {
