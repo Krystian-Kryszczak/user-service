ae2936b1b2800b1ff0567f49b84a20717882e002
Krystian Kryszczak <krystiankryszczak725@wp.pl>
2023-09-04 04:08:06 +0200

    Update UserController test.

--- src/test/kotlin/krystian/kryszczak/endpoint/UserControllerTest.kt
+++ src/test/kotlin/krystian/kryszczak/endpoint/UserControllerTest.kt
@@ -3,22 +3,21 @@ package krystian.kryszczak.endpoint
 import fixtures.user.UserFixtures
 import io.kotest.core.spec.style.StringSpec
 import io.kotest.matchers.shouldBe
+import io.micronaut.http.client.HttpClient
+import io.micronaut.http.client.annotation.Client
 import io.micronaut.test.annotation.MockBean
 import io.micronaut.test.extensions.kotest5.annotation.MicronautTest
 import io.mockk.every
 import io.mockk.mockk
 import io.reactivex.rxjava3.core.Maybe
-import io.restassured.specification.RequestSpecification
 import krystian.kryszczak.commons.model.being.user.User
 import krystian.kryszczak.commons.service.being.user.UserService
 import java.util.UUID
 
 @MicronautTest
-class UserControllerTest(private val spec: RequestSpecification): StringSpec({
+class UserControllerTest(@Client("/users") client: HttpClient): StringSpec({
     "response body should be test user json" {
-        spec
-            .`when`().get("/users/{id}", UUID.randomUUID())
-            .`as`(User::class.java) shouldBe UserFixtures.testUser
+        client.toBlocking().retrieve("/${UUID.randomUUID()}", User::class.java) shouldBe UserFixtures.testUser
     }
 }) {
     @MockBean(UserService::class)
