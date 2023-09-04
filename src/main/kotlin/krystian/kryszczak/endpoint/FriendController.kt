586ee2d7c0ecfd52b6aa113ac61a6b96ae39b0b1
Krystian Kryszczak <krystiankryszczak725@wp.pl>
2023-09-04 23:59:04 +0200

    Update friend controller.

--- src/main/kotlin/krystian/kryszczak/endpoint/FriendController.kt
+++ src/main/kotlin/krystian/kryszczak/endpoint/FriendController.kt
@@ -1,50 +1,49 @@
 package krystian.kryszczak.endpoint
 
 import io.micronaut.http.HttpResponse
-import io.micronaut.http.HttpStatus.*
-import io.micronaut.http.annotation.Controller
-import io.micronaut.http.annotation.CustomHttpMethod
-import io.micronaut.http.annotation.Delete
-import io.micronaut.http.annotation.Get
-import io.micronaut.http.annotation.Post
+import io.micronaut.http.HttpStatus.UNAUTHORIZED
+import io.micronaut.http.annotation.*
+import io.micronaut.security.annotation.Secured
 import io.micronaut.security.authentication.Authentication
+import io.micronaut.security.rules.SecurityRule
 import krystian.kryszczak.commons.utils.SecurityUtils
 import krystian.kryszczak.model.invitation.FriendInvitationModel
 import krystian.kryszczak.service.friend.FriendService
 import java.util.UUID
 
-@Controller("/friends/")
+@Secured(SecurityRule.IS_AUTHENTICATED)
+@Controller("/friends")
 class FriendController(private val friendService: FriendService) {
-    @Get("/propose")
-    fun propose(authentication: Authentication) = friendService.propose(authentication)
+    @Get("/{?page:3}")
+    fun friendshipList(page: Int?, authentication: Authentication) = useWithExtractedId(authentication) {
+        clientId -> friendService.friendshipList(page ?: 0, clientId)
+    }
+
+    @Post
+    fun invite(@Body id: UUID, authentication: Authentication) = useWithExtractedId(authentication) {
+        clientId -> friendService.sendInvitation(FriendInvitationModel(clientId, id))
+    }
+
+    @Delete
+    fun remove(@Body id: UUID, authentication: Authentication) = useWithExtractedId(authentication) {
+        clientId -> friendService.removeFriend(id, clientId)
+    }
 
     @Get("/invitations")
     fun invitations(authentication: Authentication) = useWithExtractedId(authentication, friendService::invitations)
 
-    @Get
-    fun friendshipList(page: Int, authentication: Authentication) = useWithExtractedId(authentication) {
-        clientId -> friendService.friendshipList(page, clientId)
-    }
-
-    @Post
-    fun invite(id: UUID, authentication: Authentication) = useWithExtractedId(authentication) {
-        clientId -> friendService.sendInvitation(FriendInvitationModel(clientId, id))
-    }
-
-    @CustomHttpMethod(method = "ACCEPT")
-    fun accept(id: UUID, authentication: Authentication) = useWithExtractedId(authentication) {
+    @Post("/invitations")
+    fun accept(@Body id: UUID, authentication: Authentication) = useWithExtractedId(authentication) {
         clientId -> friendService.acceptInvitation(FriendInvitationModel(id, clientId))
     }
 
-    @CustomHttpMethod(method = "DENY")
-    fun deny(id: UUID, authentication: Authentication) = useWithExtractedId(authentication) {
+    @Delete("/invitations")
+    fun deny(@Body id: UUID, authentication: Authentication) = useWithExtractedId(authentication) {
         clientId -> friendService.denyInvitation(FriendInvitationModel(id, clientId))
     }
 
-    @Delete
-    fun remove(id: UUID, authentication: Authentication) = useWithExtractedId(authentication) {
-        clientId -> friendService.removeFriend(id, clientId)
-    }
+    @Get("/propose")
+    fun propose(authentication: Authentication) = friendService.propose(authentication)
 
     private inline fun <T> useWithExtractedId(authentication: Authentication, body: (id: UUID) -> T): HttpResponse<T> {
         return HttpResponse.ok(
