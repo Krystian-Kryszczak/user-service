4d40775f7506460ff29334cc6295532e566f7cfb
Krystian Kryszczak <krystiankryszczak725@wp.pl>
2023-09-06 02:51:51 +0200

    Create collection extension.

---
+++ src/main/kotlin/krystian/kryszczak/extension/CollectionExtension.kt
@@ -0,0 +1,6 @@
+package krystian.kryszczak.extension
+
+import kotlin.math.min
+import kotlin.random.Random
+
+inline fun <reified T> MutableList<T>.takeRandom(n: Int) = List(min(n, size)) { removeAt(Random.nextInt(size - 1)) }
