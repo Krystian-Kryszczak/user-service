172b767373f36850c81560498e890c9c1eaa1cf9
Krystian Kryszczak <krystiankryszczak725@wp.pl>
2023-09-06 22:26:52 +0200

    Update user test fixtures.

--- src/test/kotlin/fixtures/user/UserFixtures.kt
+++ src/test/kotlin/fixtures/user/UserFixtures.kt
@@ -5,16 +5,29 @@ import krystian.kryszczak.commons.model.being.user.User
 import java.time.LocalDate
 import java.time.temporal.ChronoUnit
 
-object UserFixtures {
-    val testUser: User = User(
-        Uuids.timeBased(),
-        "John",
-        "Smith",
-        "john.smith@example.com",
-        "555 555 555",
-        LocalDate.EPOCH.until(LocalDate.of(2003, 7, 25), ChronoUnit.DAYS).toInt(),
-        setOf(),
-        0,
-        null
-    )
-}
+private val testUserId = Uuids.timeBased()
+private val secondTestUserId = Uuids.timeBased()
+
+val testUser = User(
+    testUserId,
+    "John",
+    "Smith",
+    "john.smith@example.com",
+    "555 555 555",
+    LocalDate.EPOCH.until(LocalDate.of(2003, 7, 25), ChronoUnit.DAYS).toInt(),
+    setOf(Uuids.timeBased()),
+    0,
+    null
+)
+
+val secondTestUser = User(
+    secondTestUserId,
+    "Jack",
+    "Smith",
+    "jack.smith@example.com",
+    "585 585 585",
+    LocalDate.EPOCH.until(LocalDate.of(2003, 7, 25), ChronoUnit.DAYS).toInt(),
+    setOf(Uuids.timeBased()),
+    0,
+    null
+)
