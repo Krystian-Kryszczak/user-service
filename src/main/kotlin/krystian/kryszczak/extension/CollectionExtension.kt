a405d10f34d75877c42869a993e718b248b089d6
Krystian Kryszczak <krystiankryszczak725@wp.pl>
2023-09-06 11:57:16 +0200

    Fix bug in collection extension.

--- src/main/kotlin/krystian/kryszczak/extension/CollectionExtension.kt
+++ src/main/kotlin/krystian/kryszczak/extension/CollectionExtension.kt
@@ -3,4 +3,4 @@ package krystian.kryszczak.extension
 import kotlin.math.min
 import kotlin.random.Random
 
-inline fun <reified T> MutableList<T>.takeRandom(n: Int) = List(min(n, size)) { removeAt(Random.nextInt(size - 1)) }
+inline fun <reified T> MutableList<T>.takeRandom(n: Int) = List(min(n, size)) { removeAt(Random.nextInt(size)) }
