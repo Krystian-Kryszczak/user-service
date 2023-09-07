8cd19dc18567746852759ba63386c9f54d0408ef
Krystian Kryszczak <krystiankryszczak725@wp.pl>
2023-09-07 21:06:39 +0200

    Update test user fixtures.

--- src/test/kotlin/fixtures/user/UserFixtures.kt
+++ src/test/kotlin/fixtures/user/UserFixtures.kt
@@ -5,8 +5,9 @@ import krystian.kryszczak.commons.model.being.user.User
 import java.time.LocalDate
 import java.time.temporal.ChronoUnit
 
-private val testUserId = Uuids.timeBased()
-private val secondTestUserId = Uuids.timeBased()
+private val testUserId = Uuids.startOf(System.currentTimeMillis() + 100)
+private val secondTestUserId = Uuids.startOf(System.currentTimeMillis() + 250)
+private val thirdTestUserId = Uuids.startOf(System.currentTimeMillis() + 500)
 
 val testUser = User(
     testUserId,
@@ -15,7 +16,7 @@ val testUser = User(
     "john.smith@example.com",
     "555 555 555",
     LocalDate.EPOCH.until(LocalDate.of(2003, 7, 25), ChronoUnit.DAYS).toInt(),
-    setOf(Uuids.timeBased()),
+    mutableSetOf(secondTestUserId),
     0,
     null
 )
@@ -27,7 +28,19 @@ val secondTestUser = User(
     "jack.smith@example.com",
     "585 585 585",
     LocalDate.EPOCH.until(LocalDate.of(2003, 7, 25), ChronoUnit.DAYS).toInt(),
-    setOf(Uuids.timeBased()),
+    mutableSetOf(testUserId, thirdTestUserId),
+    0,
+    null
+)
+
+val thirdTestUser = User(
+    thirdTestUserId,
+    "Jason",
+    "Smith",
+    "jason.smith@example.com",
+    "785 785 785",
+    LocalDate.EPOCH.until(LocalDate.of(2003, 7, 25), ChronoUnit.DAYS).toInt(),
+    mutableSetOf(secondTestUserId),
     0,
     null
 )
