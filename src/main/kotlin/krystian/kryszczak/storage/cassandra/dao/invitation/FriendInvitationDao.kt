8f5ba89ec846cc5e8b84ea2e3251f9e3111bf6eb
Krystian Kryszczak <krystiankryszczak725@wp.pl>
2023-09-09 21:44:09 +0200

    Update friend invitation dao.

--- src/main/kotlin/krystian/kryszczak/storage/cassandra/dao/invitation/FriendInvitationDao.kt
+++ src/main/kotlin/krystian/kryszczak/storage/cassandra/dao/invitation/FriendInvitationDao.kt
@@ -2,20 +2,18 @@ package krystian.kryszczak.storage.cassandra.dao.invitation
 
 import com.datastax.dse.driver.api.core.cql.reactive.ReactiveResultSet
 import com.datastax.dse.driver.api.mapper.reactive.MappedReactiveResultSet
-import com.datastax.oss.driver.api.mapper.annotations.CqlName
-import com.datastax.oss.driver.api.mapper.annotations.Dao
-import com.datastax.oss.driver.api.mapper.annotations.Delete
-import com.datastax.oss.driver.api.mapper.annotations.Select
+import com.datastax.oss.driver.api.mapper.annotations.*
+import krystian.kryszczak.commons.model.being.user.User
 import krystian.kryszczak.commons.storage.cassandra.dao.ItemDao
 import krystian.kryszczak.model.invitation.FriendInvitation
-import java.util.*
+import java.util.UUID
 
 @Dao
 interface FriendInvitationDao: ItemDao<FriendInvitation> {
     @Select(customWhereClause = "receiver = :receiver", limit = "100", allowFiltering = true)
-    fun findByReceiverId(@CqlName("receiver") receiver: UUID): MappedReactiveResultSet<FriendInvitation>
+    fun findByReceiver(@CqlName("receiver") receiver: UUID): MappedReactiveResultSet<FriendInvitation>
     @Delete(entityClass = [FriendInvitation::class])
     fun deleteById(id: UUID): ReactiveResultSet
-    @Delete(entityClass = [FriendInvitation::class], ifExists = true)
-    fun deleteByIdIfExists(id: UUID): MappedReactiveResultSet<Boolean>
+    @Delete(entityClass = [User::class], ifExists = true)
+    fun deleteByIdIfExists(id: UUID): ReactiveResultSet
 }
