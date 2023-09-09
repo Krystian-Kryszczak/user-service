16624847f20e3c495961e532c9ff775a806967cb
Krystian Kryszczak <krystiankryszczak725@wp.pl>
2023-09-09 21:45:39 +0200

    Update friend dao.

--- src/main/kotlin/krystian/kryszczak/storage/cassandra/dao/friend/FriendDao.kt
+++ src/main/kotlin/krystian/kryszczak/storage/cassandra/dao/friend/FriendDao.kt
@@ -1,20 +1,24 @@
 package krystian.kryszczak.storage.cassandra.dao.friend
 
-import com.datastax.oss.driver.api.mapper.annotations.CqlName
-import com.datastax.oss.driver.api.mapper.annotations.Dao
-import com.datastax.oss.driver.api.mapper.annotations.Query
+import com.datastax.dse.driver.api.core.cql.reactive.ReactiveResultSet
+import com.datastax.dse.driver.api.mapper.reactive.MappedReactiveResultSet
+import com.datastax.oss.driver.api.mapper.annotations.*
+import krystian.kryszczak.commons.model.being.user.User
 import krystian.kryszczak.commons.storage.cassandra.dao.being.user.UserDao
 import java.util.UUID
 
 @Dao
 interface FriendDao: UserDao {
-    @Query("UPDATE user SET friends = friends + :friendsId WHERE id = :id")
-    fun addFriend(@CqlName("id") id: UUID, @CqlName("friendId") friendId: UUID)
-    @Query("UPDATE user SET friends = friends + :friendsIds WHERE id = :id")
-    fun addFriends(@CqlName("id") id: UUID, @CqlName("friendIds") friendIds: Set<UUID>)
-
-    @Query("UPDATE user SET friends = friends - :friendsId WHERE id = :id")
-    fun removeFriend(@CqlName("id") id: UUID, @CqlName("friendId") friendId: UUID)
-    @Query("UPDATE user SET friends = friends - :friendsIds WHERE id = :id")
-    fun removeFriends(@CqlName("id") id: UUID, @CqlName("friendIds") friendIds: Set<UUID>)
+    @Select(customWhereClause = "id IN :ids")
+    fun findByIdInIds(@CqlName("ids") ids: List<UUID>): MappedReactiveResultSet<User>
+    @Select(customWhereClause = "id = :id")
+    fun findFriendsById(@CqlName("id") id: UUID): MappedReactiveResultSet<User>
+    @Select(customWhereClause = "id IN :ids")
+    fun findFriendsByIdInIds(@CqlName("ids") ids: List<UUID>): MappedReactiveResultSet<User>
+    @Select(customWhereClause = "lastname LIKE :lastname", limit = ":l")
+    fun searchByLastname(@CqlName("lastname") lastname: String, @CqlName("l") limit: Int): MappedReactiveResultSet<User>
+    @Query("UPDATE user SET friends = friends + :friendsIds WHERE id = :id;")
+    fun addFriends(@CqlName("id") id: UUID, @CqlName("friendsIds") friendsIds: Set<UUID>): ReactiveResultSet
+    @Query("UPDATE user SET friends = friends - :friendsIds WHERE id = :id;")
+    fun removeFriends(@CqlName("id") id: UUID, @CqlName("friendsIds") friendsIds: Set<UUID>): ReactiveResultSet
 }
