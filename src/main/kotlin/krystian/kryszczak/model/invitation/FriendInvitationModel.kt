db2e9e96a2acab01410587bcd7ed3dc7f91ae74a
Krystian Kryszczak <krystiankryszczak725@wp.pl>
2023-09-10 19:03:00 +0200

    Change version of project to "0.2.0".

--- src/main/kotlin/krystian/kryszczak/model/invitation/FriendInvitationModel.kt
+++ src/main/kotlin/krystian/kryszczak/model/invitation/FriendInvitationModel.kt
@@ -1,9 +1,10 @@
 package krystian.kryszczak.model.invitation
 
+import com.datastax.oss.driver.api.core.uuid.Uuids
 import io.micronaut.serde.annotation.Serdeable
 import java.util.UUID
 
 @Serdeable
 data class FriendInvitationModel(val inviter: UUID, val receiver: UUID) {
-    fun mapToInvitation() = FriendInvitation(inviter = inviter, receiver = receiver)
+    fun mapToInvitation() = FriendInvitation(Uuids.timeBased(), inviter, receiver)
 }
