da8699a71f71bcd613f62ceaa0eea282c03e4607
Krystian Kryszczak <krystiankryszczak725@wp.pl>
2023-09-13 10:42:37 +0200

    Update version of service commons library.

--- src/main/kotlin/krystian/kryszczak/extension/CollectionExtension.kt
+++
@@ -1,6 +0,0 @@
-package krystian.kryszczak.extension
-
-import kotlin.math.min
-import kotlin.random.Random
-
-inline fun <reified T> MutableList<T>.takeRandom(n: Int) = List(min(n, size)) { removeAt(Random.nextInt(size)) }
