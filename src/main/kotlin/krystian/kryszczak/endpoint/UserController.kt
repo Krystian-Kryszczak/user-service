27b74c3e8e1e8973aa6747397ca8859f8bbf26e8
Krystian Kryszczak <krystiankryszczak725@wp.pl>
2023-09-13 23:02:03 +0200

    Update user controller.

--- src/main/kotlin/krystian/kryszczak/endpoint/UserController.kt
+++ src/main/kotlin/krystian/kryszczak/endpoint/UserController.kt
@@ -1,9 +1,11 @@
 package krystian.kryszczak.endpoint
 
+import io.micronaut.http.MediaType
 import io.micronaut.http.annotation.Controller
 import io.micronaut.http.annotation.Get
 import io.micronaut.security.annotation.Secured
 import io.micronaut.security.rules.SecurityRule
+import io.reactivex.rxjava3.core.Flowable
 import io.reactivex.rxjava3.core.Maybe
 import krystian.kryszczak.commons.model.being.user.User
 import krystian.kryszczak.commons.service.being.user.UserService
@@ -12,6 +14,9 @@ import java.util.UUID
 @Secured(SecurityRule.IS_ANONYMOUS)
 @Controller("/users")
 class UserController(private val userService: UserService) {
-    @Get("/{id}")
+    @Get("/{id}", processes = [MediaType.APPLICATION_JSON])
     fun get(id: UUID): Maybe<User> = userService.findById(id)
+
+    @Get("/{ids}", processes = [MediaType.APPLICATION_JSON_STREAM])
+    fun get(ids: List<UUID>): Flowable<User> = userService.findByIdInIds(ids)
 }
